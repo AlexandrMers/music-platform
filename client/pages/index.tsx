3951e3a31c87f15373106316f854a825d364f72e
Алекандр Авдеев <alexander.avdeev@balance-pl.ru>
2021-11-25 09:15:17 +0300

    add material ui

---
+++ client/pages/index.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@material-ui/core";
+
+const Index = () => {
+  return (
+    <div>
+      <h1>Это главная страница</h1>
+      <Button>Какая-то кнопка</Button>
+    </div>
+  );
+};
+
+export default Index;
